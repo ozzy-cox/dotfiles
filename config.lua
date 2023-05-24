cea0532a4d20df48067d4189613e21ab2d2f81ff
ozank <ozankoksal@hotmail.com>
2023-05-24 23:33:49 +0300

    Updated lvim config

--- config.lua
+++ config.lua
@@ -3,12 +3,39 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
-vim.opt.mouse = ""
 
-vim.opt.shiftwidth = 2
+lvim.builtin.dap.active = true
+
+local dap = require('dap')
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function()
+  require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+end)
+
+table.insert(dap.configurations.python, {
+  type = 'python',
+  request = 'launch',
+  name = 'Django',
+  program = vim.fn.getcwd() .. '/manage.py', -- NOTE: Adapt path to manage.py as needed
+  args = { 'runserver' },
+})
+
+lvim.builtin.alpha.active = true
+
+vim.opt.mouse = ""
 vim.opt.tabstop = 2
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+
+vim.keymap.set("n", "<C-S-Up>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-S-Down>", ":resize -2<CR>")
+
+vim.keymap.set("n", "<C-S-Left>", ":vertical resize +2<CR>")
+vim.keymap.set("n", "<C-S-Right>", ":vertical resize -2<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -66,9 +93,14 @@ lvim.plugins = {
   },
   {
     'tpope/vim-fugitive'
-  }
+  },
+  "mfussenegger/nvim-dap-python",
+  "nvim-neotest/neotest",
+  "nvim-neotest/neotest-python",
+  "windwp/nvim-ts-autotag"
 }
 
+require('nvim-ts-autotag').setup()
 lvim.builtin.which_key.mappings["C"] = {
   name = "Python",
   c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
