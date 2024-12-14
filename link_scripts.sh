7a52435ee37fdb78184f1f8b91eb9562c6d82393
ozank <ozan.koksal@turboard.com>
2024-12-14 08:25:42 +0000

    Updated

--- link_scripts.sh
+++ link_scripts.sh
@@ -1,4 +1,4 @@
 ln -s ~/dotfiles/.alacritty.toml ~/.alacritty.toml
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/config/nvim-kickstart  ~/.config/nvim-kickstart
-
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
---
+++ necessities.sh
@@ -0,0 +1,6 @@
+paru -Syu lua51 luajit lazygit nerd-fonts consolas-font
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo fc-cache -f -v
+paru -Sy bash bc coreutils git jq playerctl
+
+
